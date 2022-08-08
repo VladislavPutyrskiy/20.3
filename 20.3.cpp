4801c6f312b2fe53076eebe179548ee8c7f50d3d
Vladislav Putyrskiy <V.V.Putyrskiy@gmail.com>
2022-08-09 00:48:03 +0300

    1st commit

---
+++ 20.3.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream> 
+
+
+    int main()
+    {
+        std::string qwery_fish;
+        std::string from_river;
+        int catched = 0;
+        std::cout << "What fish do you want to catch?\n";
+        std::cin >> qwery_fish;
+        std::ifstream river("river.txt");
+        std::ofstream basket("basket.txt", std::ios::app);
+        if (river.is_open() && basket.is_open())
+        {
+            while (getline(river, from_river))
+            {
+                if (qwery_fish == from_river)
+                {
+                    basket << from_river << std::endl;
+                    catched++;
+                }
+            }
+            river.close();
+            basket.close();
+        }
+        std::cout << "you have catched " << catched << " fish";
+    }
